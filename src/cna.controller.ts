ca283ba0bd4a310b53131c4a2f854238e4eb9283
Titouan Real <titouan.real@gmail.com>
2023-12-21 16:29:44 +0100

    Fix CNA service

--- src/cna.controller.ts
+++ src/cna.controller.ts
@@ -6,27 +6,22 @@ import {
 import { CnaService } from './cna.service';
 import { Cna } from './Cna';
 
-@Controller('cves')
+@Controller('cnas')
 export class CnaController {
     constructor(private readonly cnaService: CnaService) {}
 
     @Get()
-    getAllCves(): Array<Cna> {
-        return this.cnaService.getAllCves();
+    getAllCnas(): Array<Cna> {
+        return this.cnaService.getAllCnas();
     }
 
     @Get()
-    getNumberOfCves(): number {
-        return this.cnaService.getTotalNumberOfCves();
+    getNumberOfCnas(): number {
+        return this.cnaService.getTotalNumberOfCnas();
     }
 
-    @Get(':number')
-    getCvesBetween(@Param('id') id: string): Array<Cna> {
-        return this.cnaService.getAllCves();
-    }
-
-    @Get(':id')
-    getCve(@Param('id') id: string): Cna {
-        return this.cnaService.getCve(id);
+    @Get(':partner')
+    getCna(@Param('partner') partner: string): Cna {
+        return this.cnaService.getCna(partner);
     }
 }
